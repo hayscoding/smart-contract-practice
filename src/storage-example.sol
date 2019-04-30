7c4b7473db95f1be8c378ec594c9472b5b23b7a9
Hays Stanford <stanfordhays@gmail.com>
2019-04-30 12:14:21 +0000

    updated get() to be payable

--- src/storage-example.sol
+++ src/storage-example.sol
@@ -7,7 +7,8 @@ contract SimpleStorage {
         storedData = x;
     }
 
-    function get() public view returns (uint) {
+    function get() public payable returns (uint) {
+	uint256 _id = 0x420042;
 	log3(
             bytes32(msg.value),
             bytes32(0x50cb9fe53daa9737b786ab3646f04d0150dc50ef4e75f59509d83667ad5adb20),
