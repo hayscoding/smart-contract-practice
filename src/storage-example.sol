01bfb13ec96c32cf153b6df16eac50889aa3ab2d
Hays Stanford <stanfordhays@gmail.com>
2019-04-30 12:11:16 +0000

    added log3() statement in function

--- src/storage-example.sol
+++ src/storage-example.sol
@@ -8,6 +8,12 @@ contract SimpleStorage {
     }
 
     function get() public view returns (uint) {
+	log3(
+            bytes32(msg.value),
+            bytes32(0x50cb9fe53daa9737b786ab3646f04d0150dc50ef4e75f59509d83667ad5adb20),
+            bytes32(uint256(msg.sender)),
+            bytes32(_id)
+        );
         return storedData;
     }
 }
